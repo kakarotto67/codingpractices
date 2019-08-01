2943f6585bf2e30a90e973f0cd028147f4824b9b
Roman Vovk <arromanvk@gmail.com>
2019-08-01 19:01:23 +0300

    Added RabbitMQ hello world app.

---
+++ RabbitMQ/HelloWorld/MessageConsumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageConsumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rabbitMqConnectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            // 1. Setup Connection and Channel
+            using (var connection = rabbitMqConnectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // 2. Create queue to make sure it is created since we will consume from it
+                    // If the queue already created by Producer - it won't be created again
+                    var queueName = "HelloWorldQueue";
+                    channel.QueueDeclare(queueName, false, false, false, null);
+
+                    // 3. Setup Consumer
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    // 4. Add handler to Consumer's Received event to receive a message
+                    consumer.Received += (model, eventArgs) =>
+                    {
+                        var byteMessageFromBody = eventArgs.Body;
+                        var plainMessageFromBody = Encoding.UTF8.GetString(byteMessageFromBody);
+                        Console.WriteLine($" [x] Message received: {plainMessageFromBody}");
+                    };
+
+                    // 5. Bind Message Queue with Consumer, so it will listen for messages in that Queue
+                    channel.BasicConsume(queueName, true, consumer);
+
+                    Console.WriteLine(" Press any key to exit.");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/HelloWorld/MessageProducer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MessageProducer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rabbitMqConnectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            // 1. Setup Connection and Channel
+            using (var connection = rabbitMqConnectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // 2. Create Queue
+                    var queueName = "HelloWorldQueue";
+                    channel.QueueDeclare(queueName, false, false, false, null);
+
+                    // 3. Create and encode message
+                    var plainMessage = "Hello World!";
+                    var byteMessage = Encoding.UTF8.GetBytes(plainMessage);
+
+                    // 4. Publish the message event to queue created on step 2
+                    channel.BasicPublish(String.Empty, queueName, null, byteMessage);
+                    Console.WriteLine($" [x] Message sent: {plainMessage}");
+
+                    Console.WriteLine(" Press any key to exit.");
+                    Console.ReadKey();
+                }
+            }
+
+
+        }
+    }
+}
