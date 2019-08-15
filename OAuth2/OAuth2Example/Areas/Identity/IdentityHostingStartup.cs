2755ac1d4331ee002ec7a1bd965190368ef57f8d
Roman Vovk <arromanvk@gmail.com>
2019-08-15 16:50:04 +0300

    Scaffolded identity-related pages for oauth2 project.

---
+++ OAuth2/OAuth2Example/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OAuth2Example.Data;
+
+[assembly: HostingStartup(typeof(OAuth2Example.Areas.Identity.IdentityHostingStartup))]
+namespace OAuth2Example.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
